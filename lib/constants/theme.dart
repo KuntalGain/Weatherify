21b7b8851cdd79ff9ac19a89d7f8aae06dd9dedb
Kuntal Gain <101191826+KuntalGain@users.noreply.github.com>
2023-09-22 12:31:35 +0530

    Initial Commit

---
+++ lib/constants/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // background Color as per Weather Condition
+  static const Color sunnyColor = Color(0xff3eabe4);
+  static const Color rainyColor = Color(0xff5f859a);
+  static const Color stormyColor = Color(0xff5f859a);
+
+  // Text Styles
+  static const TextStyle temperature = TextStyle(
+    fontSize: 100,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+  static const TextStyle date = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+  static const TextStyle condition = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+  static const TextStyle attributes = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static const TextStyle buttonText = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff444E72),
+  );
+}
